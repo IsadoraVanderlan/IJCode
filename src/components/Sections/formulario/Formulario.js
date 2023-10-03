b0842bd67dca9dc4e8889366645eab33393889b1
Isadora Vanderlan <vanderlansantos1991@gmail.com>
2023-10-03 01:39:12 -0300

    realizado correçoes no banner

--- src/components/Sections/formulario/Formulario.js
+++ src/components/Sections/formulario/Formulario.js
@@ -6,15 +6,17 @@ const formulario = () => {
     <div>
       <div className={styles.formulario}>
         <h2>Orçamento</h2>
-        <p className={styles.pform}>Preencha o formulario para realizar um orçamento sem compromisso!</p>
+        <p className={styles.pform}>
+          Preencha o formulario para realizar um orçamento sem compromisso!
+        </p>
 
         <div className={styles.conteudoform}>
           <div className={styles.textform}>
             <h3>Contato:</h3>
 
             <ul className={styles.ulform}>
-              <li>Email: ijcode.oficial@gmail.com</li>
-              <li>WhatsApp: (11)96650-2947</li>
+              <li>ijcode.oficial@gmail.com</li>
+              <li>Whats: (11)96650-2947</li>
               <li>Instagram: @ij.code</li>
             </ul>
 
@@ -24,23 +26,25 @@ const formulario = () => {
             </p>
 
             <p>
-              Nossos preços são competitivos, você pode solicitar um orçamento sem compromisso, e encontraremos o melhor projeto para você.
+              Nossos preços são competitivos, você pode solicitar um orçamento
+              sem compromisso, e encontraremos o melhor projeto para você.
             </p>
           </div>
 
           <form className={styles.form} method="get" action="envio_dados.php">
-            <input
-              type="text"
-              name="nome"
-              placeholder="Digite seu Nome..."
-            />
+            <input type="text" name="nome" placeholder="Digite seu Nome..." />
             <input
               type="e-mail"
               name="email"
               placeholder="Digite seu e-mail..."
             />
             <textarea placeholder="Mensagem"></textarea>
-            <input className={styles.enviarform} type="submit" name="enviar" value="Enviar" />
+            <input
+              className={styles.enviarform}
+              type="submit"
+              name="enviar"
+              value="Enviar"
+            />
           </form>
         </div>
       </div>
--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -5,11 +5,10 @@ const Navbar = () => {
   return (
     <nav>
       <ul className={styles.navbar}>
+      <li><a href="">Home</a></li>
         <li><a href="">Vantagens</a></li>
-        <li><a href="">Home</a></li>
         <li><a href="">Beneficíos</a></li>
-        <li><a href="">Vantagens</a></li>
-        <li><a href="">Últimos Trabalhos</a></li>
+        <li><a href="">Orçamento</a></li>
       </ul>
     </nav>
   )
