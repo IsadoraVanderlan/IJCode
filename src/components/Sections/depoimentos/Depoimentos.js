ae7c13d03c9db873e40c46597c730e61df2a183c
Julio Cesar <julio@DESKTOP-F9ET79C>
2023-10-02 23:06:47 -0300

    Feat: Criação de Carrousel com Swiper

--- src/components/Sections/depoimentos/Depoimentos.js
+++ src/components/Sections/depoimentos/Depoimentos.js
@@ -1,39 +1,81 @@
-'use client';
-import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
-import { items } from '../../../assets/Items.json';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
-import styles from './depoimentos.module.css';
+"use client";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import styles from "./depoimentos.module.css";
+import "swiper/css";
+import "swiper/css/navigation";
+
+const cards = [
+  {
+    id: 1,
+    title: "João Carlos ",
+    subTitle: "De&Fa Uniformes",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quos mollitia sed quod consectetur at quam dolore praesentium neque eos assumenda iusto nam.",
+    imageUrl: "/img/persons/joao.svg",
+  },
+  {
+    id: 2,
+    title: "Swiper Carousel Example",
+    subTitle: "Swiper Carousel Example",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quos mollitia sed quod consectetur at quam dolore praesentium neque eos assumenda iusto nam laborum laboriosam odio blanditiis possimus accusantium recusandae porro exercitationem itaque",
+    imageUrl: "/img/persons/joao.svg",
+  },
+  {
+    id: 3,
+    title: "Swiper Carousel Example",
+    subTitle: "Swiper Carousel Example",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quos mollitia sed quod consectetur at quam dolore praesentium neque eos assumenda iusto nam laborum laboriosam odio blanditiis possimus accusantium recusandae porro exercitationem itaque",
+    imageUrl: "/img/persons/joao.svg",
+  },
+  {
+    id: 4,
+    title: "Swiper Carousel Example",
+    subTitle: "Swiper Carousel Example",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quos mollitia sed quod consectetur at quam dolore praesentium neque eos assumenda iusto nam laborum laboriosam odio blanditiis possimus accusantium recusandae porro exercitationem itaque",
+    imageUrl: "/img/persons/joao.svg",
+  },
+  {
+    id: 5,
+    title: "Swiper Carousel Example",
+    subTitle: "Swiper Carousel Example",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quos mollitia sed quod consectetur at quam dolore praesentium neque eos assumenda iusto nam laborum laboriosam odio blanditiis possimus accusantium recusandae porro exercitationem itaque",
+    imageUrl: "/img/persons/joao.svg",
+  },
+];
 
 const Depoimentos = () => {
-  const { responsive } = items;
   return (
     <div className={styles.depoimentos}>
-      <h2>Depoimentos</h2>
-      <p></p>
+      <section className={styles.title}>
+        <h2>Depoimentos</h2>
+        <p></p>
+      </section>
 
-      <Carousel
-        showArrows={true}
-        showIndicators={false}
-        infiniteLoop={false}
-        dynamicHeight={false}
-        width={'100%'}
-        className={styles.mySwiper}
-      >
-        {responsive.map((item) => (
-          <div key={item.id} className={styles.swipItem}>
-            <div className={styles.imgBox}>
-              <img src={item.imageUrl} alt="slides" />
-            </div>
-            <div className={styles.detail}>
-              <h2>{item.title}</h2>
-              <p>{item.text}</p>
-              <h3>{item.subTitle}</h3>
-            </div>
-          </div>
-          
-        ))}
-      </Carousel>
+      <section>
+        <Swiper
+          className={styles.mySwiper}
+          navigation={true}
+          modules={[Navigation]}
+          spaceBetween={200}
+          slidesPerView={3}
+        >
+          {cards.map((item) => (
+            <SwiperSlide>
+              <div className={styles.swipItem}>
+                <div className={styles.imgBox}>
+                  <img src={item.imageUrl} alt="slides" />
+                </div>
+                <div className={styles.detail}>
+                  <h2>{item.title}</h2>
+                  <h3>{item.subTitle}</h3>
+                  <p>{item.text}</p>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </section>
     </div>
   );
 };
