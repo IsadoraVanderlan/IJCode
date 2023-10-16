986d16e0ca88bac948ad0ffceba59bd2d3fad7a9
Julio Cesar <juliocesar2111@hotmail.com>
2023-10-15 21:19:27 -0300

    Fix: Corrigido menu mobile que nao fechava

--- src/components/Sections/header/Header.js
+++ src/components/Sections/header/Header.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import styles from './header.module.css';
-import Navbar from '@/components/UI/Navbar';
-import Button from '@/components/UI/Button';
-import MenuMobile from '@/components/UI/MenuMobile';
+import React from "react";
+import styles from "./header.module.css";
+import Navbar from "@/components/UI/Navbar";
+import Button from "@/components/UI/Button";
+import MenuMobile from "@/components/UI/MenuMobile";
 
 const Header = () => {
   return (
-    <header className={`${styles.main} main`} id='home'>
+    <header className={`${styles.main} main`} id="home">
       <img className={styles.logo} src="./img/logo.png" alt="" />
       <div className={styles.nav}>
         <Navbar />
